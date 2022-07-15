7a38b14da322d88299f3c15bb6aa4f03ecab80c9
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-15 22:23:44 +0200

    lab1

--- network.tf
+++ network.tf
@@ -1,7 +1,8 @@
+#creating a virtual private network with CIDR 10.0.0.0/16
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
 }
-
+#creating first public subnet with CIDR 10.0.1.0/14 in AZ us-east-1a under the new created vpc 
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -10,6 +11,7 @@ resource "aws_subnet" "public1" {
     Name = "public-subnet1"
   }
 }
+#creating second public subnet with CIDR 10.0.2.0/14 in AZ us-east-1b under the new created vpc 
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.2.0/24"
@@ -19,6 +21,8 @@ resource "aws_subnet" "public2" {
     Name = "public-subnet2"
   }
 }
+#creating first private subnet with CIDR 10.0.3.0/14 in AZ us-east-1a under the new created vpc 
+
 resource "aws_subnet" "private1" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.3.0/24"
@@ -27,6 +31,7 @@ resource "aws_subnet" "private1" {
     Name = "private-subnet1"
   }
 }
+#creating second private subnet with CIDR 10.0.4.0/14 in AZ us-east-1b under the new created vpc 
 resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.4.0/24"
