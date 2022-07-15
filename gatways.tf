c1a1618b9a634cc2c0560e343ad7a952910f038c
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-15 22:27:34 +0200

    add comments

--- gatways.tf
+++ gatways.tf
@@ -1,3 +1,4 @@
+# creating the internet gatway for public subnets
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my-vpc.id
 
@@ -5,8 +6,9 @@ resource "aws_internet_gateway" "igw" {
     Name = "igw"
   }
 }
+# creating the NAT gatway for private subnets with elastic ip id attached to it and the public subnet where it can get a public route
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip.id
+  allocation_id = aws_eip.eip.id 
   subnet_id     = aws_subnet.public1.id
 
   tags = {
@@ -14,6 +16,7 @@ resource "aws_nat_gateway" "nat" {
   }
   depends_on = [aws_internet_gateway.igw]
 }
+#the created elastic ip for the NAT gatway
 resource "aws_eip" "eip" {
   depends_on = [aws_internet_gateway.igw]
 }
