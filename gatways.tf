c04d2ea5e63a29ec5b59460f6e02c33b26d6d2cc
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-16 15:48:40 +0200

    updating dev and prod variables

--- gatways.tf
+++ gatways.tf
@@ -8,7 +8,7 @@ resource "aws_internet_gateway" "igw" {
 }
 # creating the NAT gatway for private subnets with elastic ip id attached to it and the public subnet where it can get a public route
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip.id 
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public1.id
 
   tags = {
--- instances.tf
+++ instances.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "bastionHost" {
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public1.id
   key_name                    = aws_key_pair.generated_key.key_name #attach the public key i created and saved its private one in secrets manager
-  associate_public_ip_address = true #to provide it with public ip 
-  vpc_security_group_ids           = [aws_security_group.allow_ssh.id] 
+  associate_public_ip_address = true                                #to provide it with public ip 
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   tags = {
     Name = "bastionHost"
   }
@@ -14,11 +14,11 @@ resource "aws_instance" "bastionHost" {
 #creating the private instance with same ubunto AMI in private1-subnet 
 #having security group allow any traffic on port 3000 and ssh protocol on port 22 from my vpc CIDR range only
 resource "aws_instance" "privateinstance" {
-  ami             =  var.instances_AMI
-  instance_type   =  var.instance_type
-  subnet_id       = aws_subnet.private1.id
+  ami                    = var.instances_AMI
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private1.id
   vpc_security_group_ids = [aws_security_group.allow_3000.id]
-  key_name        = aws_key_pair.generated_key.key_name #attach the public key i created and saved its private one in secrets manager
+  key_name               = aws_key_pair.generated_key.key_name #attach the public key i created and saved its private one in secrets manager
   tags = {
     Name = "private-instance"
   }
