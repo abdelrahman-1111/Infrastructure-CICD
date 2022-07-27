69ea84249333e6f842989261a453893d3c9f0cc2
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-28 01:16:40 +0200

    adding the slave playbook

---
+++ ansiblefile.tf
@@ -0,0 +1,8 @@
+# resource local_file inventory {
+#     sensitive_content = ""
+#     filename             = "${path.module}/inventory"
+#     file_permission      = 0777
+#     directory_permission = 0777
+
+# }
+
