e24eae805366977ab15a10361ba906824076e9dd
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-28 19:35:49 +0200

    automate the ssh files

--- local_files.tf
+++ local_files.tf
@@ -16,7 +16,7 @@ EOF
 }
 
 resource "local_file" "sshconfig" {
-    filename = "/var/jenkins_home/.ssh/config"
+    filename = "/root/.ssh/config"
     depends_on = [local_file.private_key]
     content = <<EOF
 Host bastion
