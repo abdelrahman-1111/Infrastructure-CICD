368e0f434178f62eab8b17fe62de4a9a0e448aec
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-30 00:59:50 +0200

    updating the taget groups

--- target_groups.tf
+++ target_groups.tf
@@ -1,13 +1,12 @@
 resource "aws_lb_target_group" "tg" {
     name     = "my-lb-tg"
-    port     = 3000
+    port     = 80
     protocol = "HTTP"
     vpc_id   = module.my_network.vpc_id
 }
 
-
 resource "aws_lb_target_group_attachment" "tg_attach" {
     target_group_arn = aws_lb_target_group.tg.arn
-    target_id        = aws_instance.bastionHost.id
+    target_id        = aws_instance.privateinstance.id
     port             = 3000
 }
